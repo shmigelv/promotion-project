146145b6c45df5dcd490d0a6fbd7b7e8b87afe30
shmigelV <shmigelvolodimir.fake@gmail.com>
2021-06-28 17:27:47 +0300

    Init commit

---
+++ src/main/java/com/shmigel/promotionproject/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.shmigel.promotionproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("test");
+    }
+
+}
